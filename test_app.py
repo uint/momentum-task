c63cea637044c95a1b42597ac1a511f89d460c72
Tomasz Kurcz <uint@users.noreply.github.com>
2025-10-29 21:09:29 +0100

    add some tests

---
+++ test_app.py
@@ -0,0 +1,67 @@
+from fastapi.testclient import TestClient
+
+from app import create_app
+from core.store.in_mem import InMemoryStore
+
+def test_list_initially_empty():
+    app = create_app(InMemoryStore())
+    client = TestClient(app)
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_add_books():
+    app = create_app(InMemoryStore())
+    client = TestClient(app)
+
+    book1 = {"ident": 0, "title": "foo", "author": "bar"}
+    book2 = {"ident": 2, "title": "baz", "author": "bax"}
+
+    response = client.post("/book", json=book1)
+    assert response.status_code == 200
+    response = client.post("/book", json=book2)
+    assert response.status_code == 200
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == [{**book1, "borrowed": None}, {**book2, "borrowed": None}]
+
+def test_add_delete_book():
+    app = create_app(InMemoryStore())
+    client = TestClient(app)
+
+    book = {"ident": 0, "title": "foo", "author": "bar"}
+
+    response = client.post("/book", json=book)
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == [{**book, "borrowed": None}]
+
+    response = client.delete("/book/0")
+    assert response.status_code == 200
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_update_book():
+    app = create_app(InMemoryStore())
+    client = TestClient(app)
+
+    book = {"ident": 0, "title": "foo", "author": "bar"}
+    borrowed = {"who": 8, "when": "2025-10-29T20:05:52.661000+00:00"}
+
+    response = client.post("/book", json=book)
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == [{**book, "borrowed": None}]
+
+    response = client.put("/book/0", json={"borrowed": borrowed})
+    assert response.status_code == 200
+
+    response = client.get("/book")
+    assert response.status_code == 200
+    assert response.json() == [{**book, "borrowed": borrowed}]
