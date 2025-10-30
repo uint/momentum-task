949b7e67977694bb9f92ac4d05a7b2164f40fc98
Tomasz Kurcz <uint@users.noreply.github.com>
2025-10-30 09:29:49 +0100

    enable postgresql configuration via env vars

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
 from sqlalchemy import create_engine
 
 from app import create_app
 from core.store.sql import SqlStore
+from core.store.in_mem import InMemoryStore
 
-engine = create_engine("sqlite://", echo=True)
-app = create_app(SqlStore(engine))
+postgres_host = os.environ.get('POSTGRES_HOST')
+postgres_db = os.environ.get('POSTGRES_DB')
+postgres_user = os.environ.get('POSTGRES_USER')
+postgres_pass = os.environ.get('POSTGRES_PASS')
+
+if postgres_host is None:
+    store = InMemoryStore()
+else:
+    engine = create_engine("postgresql://{postgres_user}:{postgres_pass}@{postgres_host}/{postgres_db}", echo=True)
+    store = SqlStore(engine)
+
+app = create_app(store)
